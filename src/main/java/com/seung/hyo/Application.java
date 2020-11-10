9ad70c9096e19653567fa602f6ab164a3ab59f39
sajuni <sajuni11@naver.com>
2020-11-10 09:24:02 +0900

    commit test

---
+++ src/main/java/com/seung/hyo/Application.java
@@ -0,0 +1,12 @@
+package com.seung.hyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    //dddd
+}
